5d112414faa49169c4d8a2bfbd676f8aa4f33e93
ARAV <alan.alv96@gmail.com>
2017-11-04 09:13:21 -0600

    Lobo

---
+++ Tests/Assets/Scripts/AnimationWolf.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationWolf : MonoBehaviour {
+	Animator anim;
+	public Transform playerObject;
+	public Transform enemyObject;
+
+
+	// Use this for initialization
+	void Start () {
+		anim =  GetComponent<Animator>();
+		anim.Play ("Wolf_walk");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float playerDistance = Vector2.Distance (enemyObject.position, playerObject.position);
+
+		//if (playerDistance <= 5) {
+			anim.Play("Wolf_run");
+		//}
+		//else{
+		//	anim.Play("Wolf_walk");
+		//}
+	}
+}
---
+++ Tests/Assets/Scripts/ChargeMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeMovement : MonoBehaviour {
+
+	//public GameObject upSprite;
+	public Transform playerObject;
+	public Transform enemyObject;
+	public Transform spawnPoint;
+
+
+	// Use this for initialization
+	void Start () {
+		//this.transform.GetComponent<Renderer>().material.color = Color.red; 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//transform.LookAt (playerObject);
+		float translation;
+		float playerDistance = Vector2.Distance (enemyObject.position, playerObject.position);
+		if (playerDistance <= 5) {
+			translation = Time.deltaTime * 10;
+			//this.transform.GetComponent<Renderer>().material.color = Color.red; 
+			transform.Translate (new Vector2 (translation, 0.0f));
+		}
+		else{
+			translation = Time.deltaTime * 5;
+			transform.Translate (new Vector2 (translation, 0.0f));	
+		}
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision){
+		Debug.Log("Collided");
+		if (collision.gameObject.name == "CharacterRobotBoy") {
+			enemyObject.position = spawnPoint.position;
+		}
+	}
+}
